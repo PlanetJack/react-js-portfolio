c69bc57e9cde1bd65e1a4c06755492fec17d9db8
Jack Kim <122494246+PlanetJack@users.noreply.github.com>
2024-12-16 15:10:05 -0700

    Created project
    Created portfolio project & finished HeroSection

---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,25 @@
+export default function HeroSection () {
+    return (
+        <section id="heroSection" className="hero--section">
+            <div className="hero--section--content--box">
+                <div className="hero--section-content">
+                    <p className="section--title"> Hello, I'm Jack</p>
+                    <h1 className="hero--section--title">
+                        <span className="hero--section-title--color">Full
+                            Stack</span> {" "}
+                        <br />
+                        Developer
+                    </h1>
+                    <p className="hero--section-description">
+                    A software developer currently residing in Calgary, Canada
+                        <br /> I am a creative problem solver, always approaching challenges with a positive mindset.
+                    </p>
+                </div>
+                <button className="btn btn-primary">Get In Touch</button>
+            </div>
+            <div className="hero--section--img">
+                <img src="./img/profile_crop_removebg.png" alt="Hero Section" />
+            </div>
+        </section>
+    );
+}
