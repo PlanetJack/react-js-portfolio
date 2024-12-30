2f9fcb4b9025a1d50676a7c3dd38538f85b50ce1
Jack Kim <122494246+PlanetJack@users.noreply.github.com>
2024-12-30 02:28:21 -0700

    Worked aboutMe & MyPortfolio

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,37 @@
+export default function AboutMe (){
+    return(
+        <section id="AboutMe" className="about--section">
+            <div className="about--section--img">
+                <img src="./img/about-me.png" alt="About Me" />
+
+            </div>
+            <div className="hero--section--content--boxabout--section--box">
+                <div className="hero--section--content">
+                    <p className="section--title">About</p>
+                    <h1
+                    className="skills-section-heading">About Me</h1>
+                    <p className="hero--section-description">
+                    I am a creative problem solver,
+                    always approaching challenges with a positive mindset.
+                    </p>
+                    <p className="hero--section-description">
+                    My proactive attitude helps me foster a collaborative and
+                    constructive environment within the team, even in the face of difficulties.
+                    Although I tend to be cautious initially, I quickly adapt to new surroundings,
+                    improving communication and cooperation as I grow more comfortable with my teammates.
+                    </p>
+                    <p className="hero--section-description">
+                    I am open to various perspectives, always seeking the best solutions
+                    while maintaining mutual respect and understanding during discussions or disagreements.
+                    With an ENFJ personality type, I naturally gravitate toward leadership and teamwork,
+                    contributing positively to the organization by encouraging
+                    active communication and considering different viewpoints.
+                    </p>
+                </div>
+            </div>
+
+        </section>
+
+
+    )
+}
