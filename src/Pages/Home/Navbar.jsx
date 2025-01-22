71d7aa10faded7b7fcc68f265134ef7b9835eab9
Jack Kim <122494246+PlanetJack@users.noreply.github.com>
2025-01-22 10:34:48 -0700

    fixed Navbar code

--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -37,18 +37,14 @@ function Navbar() {
         <div>
             <img src="./img/logo.svg" alt="Logoipsum" />
         </div>
-        <a
-        href="#" 
+        <button
         className={`nav__hamburger ${navActive ? "active" : ""}`}
-        onClick={(e) => {
-            e.preventDefault();
-            toggleNav();
-        }}
+        onClick={toggleNav}
         >
             <span className="nav__hamburger__line"> </span>
             <span className="nav__hamburger__line"> </span>
             <span className="nav__hamburger__line"> </span> 
-        </a>
+        </button>
         <div className={`navbar--items ${navActive ? "active" : ""}`}>
             <ul>
                 <li>
